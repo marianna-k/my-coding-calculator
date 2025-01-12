20a075adbd90f231f88e61f1f6bd364cd7b642d7
marianna-k <mariannakapa12@gmail.com>
2025-01-12 14:30:01 +0200

    hotfix completed

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -14,6 +14,12 @@ public class Main {
     }
 
     public static int div(int a, int b){
-        return a / b;
+        try{
+            return a / b;
+        }catch(ArithmeticException e){
+            System.out.println("Denominator must not be zero");
+            throw e;
+        }
+
     }
 }
